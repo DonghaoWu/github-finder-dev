99c809c3b058fa1dc4f59c084c2dffc8066e08dd
Donghao Wu <donghao.wu.us@gmail.com>
2020-10-25 13:44:54 -0400

    added not found page and add a link in navbar.

--- edition-2-context/src/App.js
+++ edition-2-context/src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
+import User from './components/users/User';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
-import User from './components/users/User';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/githubState';
 import AlertState from './context/alert/alertState';
@@ -23,14 +23,10 @@ function App() {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route exact path='/' render={props => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )} />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:username' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
